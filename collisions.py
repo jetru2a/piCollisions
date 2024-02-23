3c5f0ff67cec5456aa6f622a75e942aeba89e313
Jérémy <jeremytrudel78@gmail.com>
2024-02-23 17:29:48 -0500

    small change to collisions function

--- collisions.py
+++ collisions.py
@@ -1,25 +1,42 @@
+LEFT_POS = 1 
+LEFT_MASS = 1
+RIGHT_POS = 3
+RIGHT_MASS = 10
+RIGHT_SPEED = 1
 
+def Collision(leftRect, rightRect):
+    leftMass, leftSpeed = leftRect.mass, leftSpeed
+    rightMass, rightSpeed = rightRect.mass, rightRect.speed
 
+    leftFinalSpeed = (leftMass - rightMass)/(leftMass+rightMass)*leftSpeed + 2*rightMass/(leftMass+rightMass)*rightSpeed
+    rightFinalSpeed = (rightMass - leftMass)/(leftMass+rightMass)*rightSpeed + 2*leftMass/(leftMass+rightMass)*leftSpeed
+    return (leftFinalSpeed,rightFinalSpeed)
 
-class LeftRect:
-    def __init__(self):
-        self.mass = 1 #kg
-        self.pos = 1 #m from left wall
-        self.speed = 0 #m/s to the left
-
-class RightRect:
-    def __init__(self,mass):
-        self.mass = mass #kg
-        self.pos = 3 #m from left wall
-        self.speed = 1 #m/s to the left
+class Rect:
+    def __init__(self, pos, mass, speed):
+        self.pos = pos
+        self.mass = mass
+        self.speed = speed
 
 class Collisions:
     def __init__(self):
-        self.leftRect = LeftRect()
-        self.rightRect = RightRect(10)
+        self.leftRect = Rect(LEFT_POS,LEFT_MASS,0)
+        self.rightRect = Rect(RIGHT_POS,RIGHT_MASS,RIGHT_SPEED)
 
-    
     def changeMass(self, mass):
         self.rightRect.mass = mass
 
+    def restart(self):
+        self.leftRect.pos = LEFT_POS
+        self.leftRect.speed = 0
+        self.rightRect.pos = RIGHT_POS
+        self.rightRect.speed = RIGHT_SPEED
     
+    def animate(self,speed):
+        self.leftRect.pos += self.leftRect.speed/10
+        self.rightRect.pos += self.rightRect.speed/10
+        if(self.leftRect.pos >= self.rightRect):
+            self.leftRect.speed, self.rightRect.speed = Collision(self.leftRect, self.rightRect)
+        if(self.leftRect.pos <= 0):
+            self.leftRect.pos = 0
+            self.leftRect.speed *= -1
